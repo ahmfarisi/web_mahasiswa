af5edecb6fbc0b847a9651698a9ea35462591be7
Ahmad Farisi <ahmfarisi@gmail.com>
2025-08-05 20:08:13 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,25 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <!-- Latest compiled and minified CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Latest compiled JavaScript -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
 </head>
 <body>
-    <h1>Kampus Inovasi</h1>
+    <div class="container">
+
+        <div class="mt-4 p-5 bg-primary text-white rounded">
+            <h1>Kampus Inovasi</h1>
+        </div>    
+
+    </div>
+
     <hr>
     <h3>Daftar Mahasiswa</h3>
     <a href="tambah.php">Tambah</a>
